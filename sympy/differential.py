a3356cc3a786cdf5bfd8591b345146e27d36ecb7
James Zhan <zhiqiangzhan@gmail.com>
2017-10-30 14:34:02 +0800

    sanitize the codes.

--- sympy/differential.py
+++ sympy/differential.py
@@ -5,7 +5,11 @@ x = sp.symbols('x')
 
 print "sin'(x) = {0}".format(sp.diff(sp.sin(x), x))
 print "sin'(x^2) = {0}".format(sp.diff(sp.sin(x ** 2), x))
+print "log'(x) = {0}".format(sp.diff(sp.log(x), x))
 
+sigmod = (1 / (1 + sp.E ** -x))
+
+print "sigmod'(x) = {0}".format(sp.diff(sigmod, x))
 
 theta0, theta1, theta2 = sp.symbols('theta0 theta1 theta2')
 x1, x2, y = sp.symbols('x1 x2 y')
